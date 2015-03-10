3ebcf8ccb42e5890eabc98fe5b9a5da7804d93c6
sudhirios <mac12@mac12s-iMac.local>
2015-03-10 09:55:12 +0530

    Initial Commit

---
+++ AutoresizingTestApp_1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoresizingTestApp_1
+//
+//  Created by sudhirios on 3/10/15.
+//  Copyright (c) 2015 Mac-12. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AutoresizingTestApp_1/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AutoresizingTestApp_1
+//
+//  Created by sudhirios on 3/10/15.
+//  Copyright (c) 2015 Mac-12. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
